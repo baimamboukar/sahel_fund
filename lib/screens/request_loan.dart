910bbabbbeaa0f30eff2fd61ea19cf0d5e3a91b6
baimamboukar <baimamboukar@gmail.com>
2021-07-23 04:33:54 +0100

    adding labeled slider

--- lib/screens/request_loan.dart
+++ lib/screens/request_loan.dart
@@ -41,14 +41,21 @@ class RequestLoan extends ConsumerWidget {
                   children: [Text("Min Amount"), Text("Max amount")],
                 ),
               ),
-              Slider(
-                value: amount.state,
-                min: 0.0,
-                max: 100.0,
-                label: "Amount",
-                focusNode: FocusNode(),
-                onChanged: (value) => amount.state,
-              ),
+             SfSlider(
+       min: 0.0,
+       max: 100.0,
+       value: _value,
+       interval: 20,
+       showTicks: true,
+       showLabels: true,
+       enableTooltip: true,
+       minorTicksPerInterval: 1,
+       onChanged: (dynamic value){
+         setState(() {
+           _value = value;
+         });
+       },
+     ),
               Padding(
                 padding: const EdgeInsets.only(left: 25.0, right: 25.0),
                 child: Row(
